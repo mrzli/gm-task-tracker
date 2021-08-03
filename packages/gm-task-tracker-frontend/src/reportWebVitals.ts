abf8da18f0ed67ac4d67c1ff545fc768829978e4
Goran Mržljak <goran.mrzljak@gmail.com>
2021-08-03 17:15:30 +0200

    refactor(frontend): clean up reportWebVital file

--- packages/gm-task-tracker-frontend/src/reportWebVitals.ts
+++ packages/gm-task-tracker-frontend/src/reportWebVitals.ts
@@ -1,15 +1,16 @@
-import { ReportHandler } from 'web-vitals';
+import { ReportHandler } from 'web-vitals/src/types';
+import { emptyFn } from '@mrzli/gm-js-libraries-utilities/function';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
+export function reportWebVitals(onPerfEntry: ReportHandler | undefined): void {
+  if (onPerfEntry) {
+    import('web-vitals')
+      .then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
+        getCLS(onPerfEntry);
+        getFID(onPerfEntry);
+        getFCP(onPerfEntry);
+        getLCP(onPerfEntry);
+        getTTFB(onPerfEntry);
+      })
+      .finally(emptyFn);
   }
-};
-
-export default reportWebVitals;
+}
