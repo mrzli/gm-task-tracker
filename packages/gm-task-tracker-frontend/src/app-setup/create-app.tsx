64a4817697a3f74a13cbaf6115390e39459444d9
Goran Mržljak <goran.mrzljak@gmail.com>
2021-08-03 19:17:03 +0200

    refactor(frontend): move app creation to a separate file
    This new file for app creation is located under 'src/app-setup'.

---
+++ packages/gm-task-tracker-frontend/src/app-setup/create-app.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { App } from '../components/App';
+// import { wrapComponent } from './create-app-utils';
+// import { createAppDependencies } from './app-dependencies';
+// import { createInitialAppState } from '../store/base/app-state';
+// import { createAppAllParameters } from './app-all-parameters';
+// import { createAppGlobals } from './app-globals';
+import { reportWebVitals } from './report-web-vitals';
+
+export function createApp(): React.ReactElement {
+  const component = <App />;
+
+  // const dependencies = createAppDependencies();
+  // const globals = createAppGlobals();
+  // const initialAppState = createInitialAppState();
+  //
+  // const appAllParameters = createAppAllParameters(
+  //   dependencies,
+  //   globals,
+  //   initialAppState
+  // );
+  //
+  // return wrapComponent(
+  //   component,
+  //   appAllParameters.appContextData,
+  //   appAllParameters.store,
+  //   appAllParameters.theme,
+  //   false,
+  //   ''
+  // );
+
+  return component;
+}
+
+export function setupApp(app: React.ReactElement): void {
+  ReactDOM.render(app, document.getElementById('root'));
+
+  // If you want to start measuring performance in your app, pass a function
+  // to log results (for example: reportWebVitals(console.log))
+  // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+  reportWebVitals(undefined);
+}
