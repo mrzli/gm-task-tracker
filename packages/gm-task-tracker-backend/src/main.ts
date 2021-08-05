87739ca7925c16d8ec361f80516e7ccc81a9cb64
Goran Mržljak <goran.mrzljak@gmail.com>
2021-08-05 14:37:38 +0200

    feat(backend): port is now read from env variable

--- packages/gm-task-tracker-backend/src/main.ts
+++ packages/gm-task-tracker-backend/src/main.ts
@@ -2,14 +2,19 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { emptyFn } from '@mrzli/gm-js-libraries-utilities/function';
 import { config as configDotEnv } from 'dotenv';
+import { getAppEnv } from './utils/app-env';
+import { parseInteger } from '@mrzli/gm-js-libraries-utilities/number';
 
 configDotEnv();
 
 async function bootstrap(): Promise<void> {
+  const env = getAppEnv();
+  const port = parseInteger(env.PORT);
+
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   app.setGlobalPrefix('api');
-  await app.listen(4000);
+  await app.listen(port);
 }
 
 bootstrap().finally(emptyFn);
