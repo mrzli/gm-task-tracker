e617b3105cd43453b55a24accaeff1d7132d095b
Goran Mržljak <goran.mrzljak@gmail.com>
2021-08-06 20:44:30 +0200

    chore(project): make docker db start interactive

--- db/dev-start.sh
+++ db/dev-start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 PROJ_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/.." >/dev/null 2>&1 && pwd)"
 
-docker compose -f "${PROJ_DIR}/db/docker-compose.db.yml" -p gm-task-tracker up -d
+docker compose -f "${PROJ_DIR}/db/docker-compose.db.yml" -p gm-task-tracker up
