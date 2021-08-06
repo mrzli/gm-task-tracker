fbd0c75067be166d812e552c0a58272f3ab73ff8
Goran Mržljak <goran.mrzljak@gmail.com>
2021-08-06 20:37:31 +0200

    feat(project): move db local setup from backend to root

--- packages/gm-task-tracker-backend/db/dev-stop.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-PROJ_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )/../../.." >/dev/null 2>&1 && pwd)"
-
-docker compose -f "${PROJ_DIR}/packages/gm-task-tracker-backend/db/docker-compose.db.yml" -p gm-task-tracker down
